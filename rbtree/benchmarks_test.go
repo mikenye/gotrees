70aad0cafbc8be416fe2981003f0fe0088f1ef73
Mike Nye <mike.nye@gmail.com>
2025-03-11 10:07:34 +0800

    better comments on tests

--- rbtree/benchmarks_test.go
+++ rbtree/benchmarks_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 )
 
+// BenchmarkTree_SearchDelete creates a very large tree (10M nodes),
+// then deletes items from said tree in the benchmarking loop.
 func BenchmarkTree_SearchDelete(b *testing.B) {
 	var n *bst.Node[int, struct{}, Color]
 
@@ -18,8 +20,8 @@ func BenchmarkTree_SearchDelete(b *testing.B) {
 		tree.Insert(i, struct{}{})
 	}
 
-	// delete
 	i := 0
+	b.ResetTimer()
 	for b.Loop() {
 		n, _ = tree.Search(i)
 		tree.Delete(n)
@@ -27,12 +29,13 @@ func BenchmarkTree_SearchDelete(b *testing.B) {
 	}
 }
 
+// BenchmarkTree_Insert creates inserts items into a tree in the benchmarking loop.
 func BenchmarkTree_Insert(b *testing.B) {
-	// create a tree with integer key & no value,
 	tree := New[int, struct{}](func(a, b int) bool {
 		return a < b
 	})
 	i := 0
+	b.ResetTimer()
 	for b.Loop() {
 		tree.Insert(i, struct{}{})
 		i++
